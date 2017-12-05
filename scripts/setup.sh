8b27d8625d9ae12de74aed92cdf07cf15db14243
cworrell <cworrell@amazon.com>
2017-12-05 17:28:03 -0500

    Remove + to space, minor updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # generate new RSA PEM key
-openssl genrsa 2048 > private_encrypted_key.pem
+openssl genrsa -out private_key.pem 2048
 
 # export public key
-openssl rsa -in private_encrypted_key.pem -outform PEM -pubout -out public_key.pem
-
-# export private key
-openssl rsa -in private_encrypted_key.pem -out private_unencrypted_key.pem -outform PEM
+openssl rsa -in private_key.pem -outform PEM -pubout -out public_key.pem
 
 # store the private key in Systems Manager Parameter Store
 aws ssm put-parameter --type "SecureString" --name "/cloudfront/field-encryption/private-key" --value file://private_unencrypted_key.pem  --key-id "$KMSKEY"
